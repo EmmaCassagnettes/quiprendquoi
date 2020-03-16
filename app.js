0aeb112aeaa5fdb922e829b12a113748af96b9d9
emmacassagnettes <e.cassagnettes@gmail.com>
2020-03-17 00:35:30 +0100

    Partie 10 - La route POST

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Une part de tarte ?' });
 });
 
+app.post('/party', (req, res) => {
+  res.send('Post ok !');
+});
+
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
