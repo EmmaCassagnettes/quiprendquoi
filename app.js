ceab72e7d641e774f85e16ec8a1a3f5f1c7a7b43
emmacassagnettes <e.cassagnettes@gmail.com>
2020-03-17 00:22:58 +0100

    Partie 8 - Passer des variables aux templates

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { title: 'Une part de tarte ?' });
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
