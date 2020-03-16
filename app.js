cd89c7a8799dec06f74d27b09c504cad3629197e
emmacassagnettes <e.cassagnettes@gmail.com>
2020-03-16 23:54:01 +0100

    Partie 4 - Rafraîchissement automatique

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Télétravail !!');
+  res.send('Hello !');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
